279d11ccc7568d60959bb164f6fb4677fb5e304c
Siddhartha Basu <biosidd@gmail.com>
2013-06-26 10:30:22 -0500

    broken session controller, so added  bunch of print statement

--- user_sessions_controller.go
+++ user_sessions_controller.go
@@ -1,5 +1,7 @@
 package gobioweb
 
+import "fmt"
+
 var SessionName string = "user_session"
 
 func (c *Controller) SaveUserSession(u *User) error {
@@ -7,6 +9,8 @@ func (c *Controller) SaveUserSession(u *User) error {
 	if err != nil {
 		return err
 	}
+
+	fmt.Printf("got id to save %d\n",u.Id)
 	session.Values["user_id"] = u.Id
 	session.Save(c.Request, c.Response)
 	return nil
@@ -31,9 +35,11 @@ func (c *Controller) CurrentUser() (*User, error) {
 	}
 
 	if id, ok := session.Values["user_id"]; ok {
+		fmt.Printf("got id %d\n",id)
 		u := &User{Id: id.(int64)}
-		u.FindById(c.App.Database)
-		return u, nil
+		newuser,_ := u.FindById(c.App.Database)
+		fmt.Printf("name:%s email:%s\n",newuser.FirstName,newuser.Email)
+		return newuser, nil
 	}
 	return nil, nil
 }
